6aebebbd19cd5b3ef9cf95e39dc997e8e12eb165
james <jamesbhattarai14@gmail.com>
2024-04-26 17:24:58 +0545

    Fix: build error with module

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,11 @@ app.get('/', (req:Request, res:Response) => {
     res.send('Hello, world!');
 });
 
+app.post("/code",(req:Request,res:Response)=>{
+    console.log("received request",req.body)
+    res.status(200).json("ok")
+})
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
